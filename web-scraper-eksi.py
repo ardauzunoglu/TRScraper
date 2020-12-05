0aed674287d7a271277f3016b11d7b4ea9e00858
Arda Uzunoğlu <ardauzunogluarda@gmail.com>
2020-12-05 16:32:15 +0300

    Add files via upload

--- web-scraper-eksi.py
+++ web-scraper-eksi.py
@@ -1,90 +1,118 @@
 import time
 import pandas as pd
+import sys
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-baslik = input("Entrylerin çekileceği başlık: ")
-dosya_adi = input("Oluşturulacak Excel dosyasının adı: ")
-dosya_adi = dosya_adi + ".xlsx"
-entry_texts = []
-author_texts = []
-date_texts = []
-scrape_date = True
-scrape_author = True
-path = "C:\Program Files (x86)\chromedriver.exe"
+def initialize():
 
-driver = webdriver.Chrome(path)
+    global baslik, dosya_adi, delay, entry_texts, author_texts, date_texts, scrape_author, scrape_date, path
 
-time.sleep(1)
+    baslik = input("Entrylerin çekileceği başlık: ")
+    dosya_adi = input("Oluşturulacak Excel dosyasının adı: ")
+    dosya_adi = dosya_adi + ".xlsx"
+    delay = int(input("Bekleme süresi: "))
+    entry_texts = []
+    author_texts = []
+    date_texts = []
+    scrape_author = True
+    scrape_date = True
+    path = "C:\Program Files (x86)\chromedriver.exe"
 
-driver.get("https://eksisozluk.com")
-
-time.sleep(1)
-
-driver.maximize_window()
-
-time.sleep(1)
-
-arama_bari = driver.find_element_by_id("search-textbox")
-arama_bari.send_keys(baslik)
-arama_bari.send_keys(Keys.ENTER)
-
-time.sleep(1)
-
-baslik_uzunlugu = driver.find_element_by_class_name("last")
-baslik_uzunlugu = int(baslik_uzunlugu.text)
-
-l = 1
-
-while l <= baslik_uzunlugu:
-            
-    time.sleep(1)
-
-    entries = driver.find_elements_by_css_selector(".content")
-
-    for entry in entries:
-        entry = entry.text
-        entry_texts.append(entry)
-
-    time.sleep(1)
-
-    dates = driver.find_elements_by_class_name("entry-date")
-                
-    for date in dates:
-        date = date.text
-        date_texts.append(date)
-
-    time.sleep(1)
-
-    authors = driver.find_elements_by_class_name("entry-author")
-
-    for author in authors:
-        author = author.text 
-        author_texts.append(author)
-
-    l += 1
-
+def scrape():
     try:
-        reklami_gec = driver.find_element_by_id("interstitial-close-link-tag")
-        reklami_gec.click()
-        time.sleep(1)
+        driver = webdriver.Chrome(path)
+        time.sleep(delay)
 
     except:
+        print("Chromedriver kullanılamıyor.")
+        sys.exit()
 
-        sonraki = driver.find_element_by_class_name("next")
-        sonraki.click()
+    try:
+        driver.get("https://eksisozluk.com")
+        time.sleep(delay)
+        driver.maximize_window()
+        time.sleep(delay)
 
-driver.close()
+    except:
+        print("Ekşi sözlüğe erişilemiyor.")
+        sys.exit()
+    try:
+        arama_bari = driver.find_element_by_id("search-textbox")
+        arama_bari.send_keys(baslik)
+        arama_bari.send_keys(Keys.ENTER)
 
-df = pd.DataFrame({"Entryler": entry_texts})
+        time.sleep(delay)
 
-if scrape_date:
-    df["Tarihler"] = date_texts
+    except:
+        print("Başlık bulunamadı.")
+        sys.exit()
 
-if scrape_author:
-    df["Yazarlar"] = author_texts
+    try:
+        baslik_uzunlugu = driver.find_element_by_class_name("last")
+        baslik_uzunlugu = int(baslik_uzunlugu.text)
 
-df.to_excel(dosya_adi, header = True, index = False)
+    except:
+        baslik_uzunlugu = 1
 
-x = "Çektiğiniz veriler "+ dosya_adi + " adlı excel dosyasına kaydedildi."
-print(x)
+    l = 1
+
+    while l <= baslik_uzunlugu:
+                
+        time.sleep(delay)
+
+        entries = driver.find_elements_by_css_selector(".content")
+
+        for entry in entries:
+            entry = entry.text
+            entry_texts.append(entry)
+
+        time.sleep(delay)
+
+        dates = driver.find_elements_by_class_name("entry-date")
+                    
+        for date in dates:
+            date = date.text
+            date_texts.append(date)
+
+        time.sleep(delay)
+
+        authors = driver.find_elements_by_class_name("entry-author")
+
+        for author in authors:
+            author = author.text 
+            author_texts.append(author)
+
+        l += 1
+
+        try:
+            reklami_gec = driver.find_element_by_id("interstitial-close-link-tag")
+            reklami_gec.click()
+            time.sleep(delay)
+
+        except:
+            try:
+                sonraki = driver.find_element_by_class_name("next")
+                sonraki.click()
+
+            except:
+                pass
+
+    driver.close()
+
+    df = pd.DataFrame({"Entryler": entry_texts})
+
+    if scrape_date:
+        df["Tarihler"] = date_texts
+
+    if scrape_author:
+        df["Yazarlar"] = author_texts
+
+    df.to_excel(dosya_adi, header = True, index = False)
+
+    x = "Çektiğiniz veriler "+ dosya_adi + " adlı excel dosyasına kaydedildi."
+    print(x)
+
+if __name__ == "__main__":
+    initialize()
+    scrape()
